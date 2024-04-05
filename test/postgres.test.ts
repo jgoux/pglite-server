b3d3e5d408be54e53404d337dc699d2e5538adb2
jgoux <hi@jgoux.dev>
2024-04-05 22:07:43 +0200

    change test name

--- test/postgres.test.ts
+++ test/postgres.test.ts
@@ -1,5 +1,5 @@
 import { test } from "./test.js";
 
-test("Postgres.js", async ({ database }) => {
+test("is provided database server and client", async ({ database }) => {
 	await database.client.sql`SELECT 1`;
 });
